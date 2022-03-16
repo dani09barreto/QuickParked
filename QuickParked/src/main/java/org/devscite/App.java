c4a63bf633f272c570d1999e51bc69f8df80eb2e
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-03-16 15:30:31 -0500

    Se añade proyecto QuickParked

---
+++ QuickParked/src/main/java/org/devscite/App.java
@@ -0,0 +1,13 @@
+package org.devscite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Satu maricon" );
+    }
+}
