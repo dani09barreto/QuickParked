8ee379f05f93d97a67252bddf09ec4e0a9aa2ded
Santiago-Xave <73509100+Santiago-Xave@users.noreply.github.com>
2022-03-23 07:33:41 -0500

    Gui Modificar

--- QuickParked/src/main/java/org/devscite/View/Controller/ControllerModifyVehicle.java
+++ QuickParked/src/main/java/org/devscite/View/Controller/ControllerModifyVehicle.java
@@ -1,6 +1,12 @@
 package org.devscite.View.Controller;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.devscite.Controller.ControllerParking;
 
 public class ControllerModifyVehicle {
@@ -10,4 +16,32 @@ public class ControllerModifyVehicle {
         return controllerParking;
     }
 
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private TextArea Platelist;
+
+    @FXML
+    private TextField plateIn;
+
+    @FXML
+    private Button btnModify;
+
+    @FXML
+    private ChoiceBox<?> VehicleTypes;
+
+    @FXML
+    void MakeModify(ActionEvent event) {
+        try{
+
+        }catch (){
+            
+        }
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
 }
