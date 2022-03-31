33a396256faf4e4d820c558c6e270fd76f5edca6
Ángel Talero <angelgotalero@outlook.com>
2022-03-30 19:33:44 -0500

    Añadida función que autogenera 50 vehículos de pruebas

--- QuickParked/src/main/java/org/devscite/View/App.java
+++ QuickParked/src/main/java/org/devscite/View/App.java
@@ -5,21 +5,59 @@ import javafx.stage.Stage;
 import org.devscite.Controller.ControllerParking;
 import org.devscite.Model.Car;
 import org.devscite.Model.CarModel;
+import org.devscite.Model.MotorCycle;
+import org.devscite.Utils.Exeptions.InvalidLicensePlate;
+import org.devscite.Utils.Exeptions.ParkingFull;
 import org.devscite.Utils.ViewType;
 import org.devscite.View.Controller.ControllerViewParking;
 import org.devscite.View.Controller.ViewManagementController;
 
 import java.util.Calendar;
+import java.util.Random;
 
 public class App extends Application {
 
     ViewManagementController<ControllerParking> viewManager = new ViewManagementController<>(new ControllerParking());
 
+    private void generateDebugData() {
+        //TODO: DEBUG
+        Random random = new Random();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String randomPlate;
+
+        for (int i = 0; i < 50; i++) {
+            switch (random.nextInt(2)) {
+                case 0:
+                    randomPlate = String.valueOf(new char[]{
+                            alphabet.charAt(random.nextInt(alphabet.length())),
+                            alphabet.charAt(random.nextInt(alphabet.length())),
+                            alphabet.charAt(random.nextInt(alphabet.length()))}) + random.nextInt(1000);
+                    try {
+                        viewManager.getController().getControllerVehicle().addVehicle(new Car(randomPlate, Calendar.getInstance(), CarModel.Automovil));
+                    } catch (Exception ignored) {
+
+                    }
+                    break;
+                case 1:
+                    randomPlate = String.valueOf(new char[]{
+                            alphabet.charAt(random.nextInt(alphabet.length())),
+                            alphabet.charAt(random.nextInt(alphabet.length())),
+                            alphabet.charAt(random.nextInt(alphabet.length()))}) + random.nextInt(100) + alphabet.charAt(random.nextInt(alphabet.length()));
+                    try {
+                        viewManager.getController().getControllerVehicle().addVehicle(new MotorCycle(randomPlate, Calendar.getInstance()));
+                    } catch (Exception ignored) {
+
+                    }
+                    break;
+            }
+
+        }
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
 
-        //TODO: DEBUF
-        viewManager.getData().getControllerVehicle().addVehicle(new Car("ABC123", Calendar.getInstance(), CarModel.Automovil));
+        generateDebugData();
 
         viewManager.createView(
                 ControllerViewParking.MAIN_FXML_NAME,
