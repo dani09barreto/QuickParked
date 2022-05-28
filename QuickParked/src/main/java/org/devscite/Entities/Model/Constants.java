087b513c918aef0cb4a4e3a9c3652f2473a0d93b
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-05-27 21:27:23 -0500

    Entrega 2

--- QuickParked/src/main/java/org/devscite/Entities/Model/Constants.java
+++ QuickParked/src/main/java/org/devscite/Entities/Model/Constants.java
@@ -1,7 +1,7 @@
 package org.devscite.Entities.Model;
 
 public class Constants {
-    public static final String USERNAME = "is101004";
+    public static final String USERNAME = "is1010";
     public static final String PASSWORD = "----------";
     public static final String THINCONN = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521/LAB";
 }
